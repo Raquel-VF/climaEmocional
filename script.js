e8bc096a317e4beb059d709afea98d5408b15d6b
Raquel-VF <raquelvargas0905@gmail.com>
2026-08-22 11:42:56 -0300

    voltando depois de um tempo

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ inputCidade.addEventListener("keypress", function(event){
     }
 });
 
+// Função para buscar o clima
 botao.addEventListener("click", async function(){
     const cidade = document.querySelector("#cidade").value;
 
@@ -20,46 +21,40 @@ botao.addEventListener("click", async function(){
 
     resultado.innerHTML ="<p>Buscando clima... ⏳</p>";
 
+// Verificar se a resposta foi bem-sucedida
     const dados = await resposta.json();
-
-    const agora = dados.dt;
-    const nascerSol = dados.sys.sunrise;
-    const porSol = dados.sys.sunset;
-
-    let periodo = "";
-    if(agora >= nascerSol && agora < porSol){
-        periodo = "dia";
-    }
-    else{
-        periodo ="noite";
-    }
-
+    
     if(dados.cod == "404"){
         resultado.innerHTML = "<p>Cidade não encontrada. Por favor, tente novamente. ❌</p>";
         return; 
     }
 
+// Analisar o clima e personalizar a experiência emocional    
     const clima = dados.weather[0].main;
-
+    
     const icone = dados.weather[0].icon;
 
-    const urlIcone = `https://openweathermap.org/img/wn/${icone}@2x.png`;
-
+    let periodo = icone.includes("n") ? "noite" : "dia";
+    
     let emojiClima = "";
-
+    
     let frase = "";
+    
+    const urlIcone = `https://openweathermap.org/img/wn/${icone}@2x.png`;
 
     // Personalização do clima emocional
     if(clima === "Clear"){
 
         if(periodo === "dia"){
 
-            document.body.style.background = "linear-gradient(180deg, #FFD166, #F4A261)";
+            const background = document.getElementById("background");
+            background.className = "sol";
             emojiClima = "☀️";
             frase = "O céu parece leve hoje ☀️";
         }
         else{
-            document.body.style.background = "linear-gradient(180deg, #1b1b2f, #0B1020)";
+            const background = document.getElementById("background");
+            background.className = "noite";
             emojiClima = "🌙";
             frase =
             "A noite também pode trazer calma 🌙";
@@ -69,26 +64,28 @@ botao.addEventListener("click", async function(){
     else if(clima === "Rain" || clima === "Drizzle"){
         emojiClima = "🌧️";
             frase = "Dias chuvosos também merecem acolhimento 🌧";
-        document.body.style.background = "linear-gradient(180deg, #1B1B2F, #0B1020)";
+        const background = document.getElementById("background");
+        background.className = "chuva";
     }
     else if(clima === "Clouds"){
         emojiClima = "☁️";
         frase = "Nem todo céu precisa estar limpo para ser bonito ☁️!";
-        document.body.style.background = "linear-gradient(180deg, #6C757D, #495057)";
-    }
-
-    else if(clima === "Rain" || clima === "Drizzle"){
-        emojiClima = "🌧️";
-    
+        const background = document.getElementById("background");
+        background.className = "nuvens";
     }
 
     console.log(clima);
 
     resultado.innerHTML = `
-    <img src = "${urlIcone}">
-    <h2>${dados.name}</h2>
-    <p>${dados.main.temp.toFixed(0)}°C</p>
-    <p>${dados.weather[0].description}</p>
+    <div id="cidade_resultado">
+        <h2 id="cidade-resultado">${dados.name}</h2>
+    </div>
+
+    <div id="temp_resultado_descricao">
+        <img src = "${urlIcone}" id="icone-resultado">
+        <p>${dados.main.temp.toFixed(0)}°C</p>
+        <p>${dados.weather[0].description}</p>
+    </div>
     <p>${frase}</p>
     `;
 });
